4c1956ad08b24a07575f5c456a27706176b823f9
Venelin Atanasov <venelin.atanasov@scalefocus.com>
2019-08-27 12:19:01 +0300

    adds event listener on save button and some input variables

--- src/app/add-reminder/add-reminder.page.ts
+++ src/app/add-reminder/add-reminder.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-add-reminder',
-  templateUrl: './add-reminder.page.html',
-  styleUrls: ['./add-reminder.page.scss'],
+    selector: 'app-add-reminder',
+    templateUrl: './add-reminder.page.html',
+    styleUrls: ['./add-reminder.page.scss']
 })
 export class AddReminderPage implements OnInit {
+    NameInput = '';
+    NotesInput = '';
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
+    SubmitInput() {
+        console.log(this.NameInput);
+        console.log(this.NotesInput);
+    }
 }
